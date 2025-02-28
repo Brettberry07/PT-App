49440bf52be3a6bbe4149fe962526019beaa9594
Brett Berry <Brettberry07@gmail.com>
2025-02-27 20:31:25 -0600

    deleting the yaml file because I hate yaml files. Anyways it builds now

---
+++ platforms/android/app/src/main/assets/app/runtime.79caf3018a677677be5f.hot-update.js
@@ -0,0 +1,14 @@
+"use strict";
+exports.id = "runtime";
+exports.ids = null;
+exports.modules = {};
+exports.runtime =
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ /* webpack/runtime/getFullHash */
+/******/ (() => {
+/******/ 	__webpack_require__.h = () => ("f9f11e61b566a07a133c")
+/******/ })();
+/******/ 
+/******/ }
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoicnVudGltZS43OWNhZjMwMThhNjc3Njc3YmU1Zi5ob3QtdXBkYXRlLmpzIiwibWFwcGluZ3MiOiI7Ozs7Ozs7O1VBQUEiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9QVC1BcHAvd2VicGFjay9ydW50aW1lL2dldEZ1bGxIYXNoIl0sInNvdXJjZXNDb250ZW50IjpbIl9fd2VicGFja19yZXF1aXJlX18uaCA9ICgpID0+IChcImY5ZjExZTYxYjU2NmEwN2ExMzNjXCIpIl0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9
